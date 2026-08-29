555406434246de30405427c74535da78ac2e1a0b
pranavabits129 <f20250129@pilani.bits-pilani.ac.in>
2026-08-29 13:58:38 +0000

    lab01 task2

--- labs/lab01/task2/ripple_adder.v
+++ labs/lab01/task2/ripple_adder.v
@@ -12,30 +12,46 @@
 //
 // Use named port connections (.a(...), .b(...), etc.), not positional.
 
-// ripple_adder.v
-module ripple_adder #(parameter WIDTH = 4) (
-  input  [WIDTH-1:0] a,
-  input  [WIDTH-1:0] b,
-  input               cin,
-  output [WIDTH-1:0] sum,
-  output              cout
+module ripple_adder(
+  input  [3:0] a,
+  input  [3:0] b,
+  input        cin,
+  output [3:0] sum,
+  output       cout
 );
-  wire [WIDTH:0] carry;
-  assign carry[0] = cin;
 
-  genvar i;
-  generate
-    for (i = 0; i < WIDTH; i = i + 1) begin : FA_STAGE
-      FA_Gate fa_inst (
-        .a    (a[i]),
-        .b    (b[i]),
-        .cin  (carry[i]),
-        .sum  (sum[i]),
-        .cout (carry[i+1])
-      );
-    end
-  endgenerate
+  wire c1, c2, c3;
 
-  assign cout = carry[WIDTH];
+  FA_Gate FA0 (
+    .a    (a[0]),
+    .b    (b[0]),
+    .cin  (cin),
+    .sum  (sum[0]),
+    .cout (c1)
+  );
+
+  FA_Gate FA1 (
+    .a    (a[1]),
+    .b    (b[1]),
+    .cin  (c1),
+    .sum  (sum[1]),
+    .cout (c2)
+  );
+
+  FA_Gate FA2 (
+    .a    (a[2]),
+    .b    (b[2]),
+    .cin  (c2),
+    .sum  (sum[2]),
+    .cout (c3)
+  );
+
+  FA_Gate FA3 (
+    .a    (a[3]),
+    .b    (b[3]),
+    .cin  (c3),
+    .sum  (sum[3]),
+    .cout (cout)
+  );
 
 endmodule
